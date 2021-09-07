1e065ba391a3ee362d4d411fc331fe8f90356929
Abner <abnercabrera28@gmail.com>
2021-09-07 01:22:41 -0400

    commit tree 0c5a1afa22c86187917427ba068014a9b629f67d

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie => 
+          <div>
+            {movie.title} ({movie.time} min)
+            {movie.genres.map(genre =>
+              <ul>
+                <li key={genre}>{genre}</li>
+              </ul>
+              )}
+          </div>
+        )}
     </div>
   );
 };
